5eae8756fa209a6fc778e6f2f237723f98e2b997
sujan khatri <sujansince2059@gmail.com>
2023-08-17 18:01:34 +0545

    profile card with react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import { StrictMode } from "react"
+import { createRoot } from "react-dom/client"
+import "./App.css"
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="card">
+      <Avatar imgsrc="https://i.postimg.cc/vBHJMqbz/helloboi.jpg" />
+      <div className="data">
+        <Intro />
+
+        <SkillList />
+      </div>
     </div>
-  );
+  )
+}
+const Avatar = (props) => {
+  return (
+    <div>
+      <img className="avatar" src={props.imgsrc} alt="hello" />
+    </div>
+  )
 }
 
-export default App;
+const Intro = () => {
+  return (
+    <div>
+      <h3 style={{ fontSize: "30px" }} className="name">
+        Cool boi 123
+      </h3>
+      <p className="info">
+        Hello i am cool boi😎,a aspiring web developer from Mars.Currently i am
+        learning React JS library.this is profile card project
+      </p>
+    </div>
+  )
+}
+const SkillList = () => {
+  return (
+    <div>
+      <ul className="skill-list">
+        <Skill skill="Javascript" bg="steelblue" pk="gg" />
+        {/* passing props as css properties value */}
+        <Skill skill="React" bg="#FAEBD7" />
+        <Skill skill="Tailwind CSS" bg="steelblue" />
+        <Skill skill="HTML" bg="#FAEBD7" />
+        <Skill skill="CSS" bg="steelblue" />
+      </ul>
+    </div>
+  )
+}
+function Skill({ skill, bg, pk }) {
+  return (
+    <li className="skill" style={{ backgroundColor: bg }}>
+      {/* using props as css values */}
+      {skill} {pk}
+    </li>
+  )
+}
+const rootElement = document.getElementById("root")
+const root = createRoot(rootElement)
+
+root.render(
+  <StrictMode>
+    <App />
+  </StrictMode>
+)
