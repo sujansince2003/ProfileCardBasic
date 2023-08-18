65871130f4d83ad95f2401ea118a660d560da809
sujan khatri <sujansince2059@gmail.com>
2023-08-18 07:43:25 +0545

    completed the challange

--- src/App.js
+++ src/App.js
@@ -1,6 +1,38 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./App.css"
+const skills = [
+  {
+    skill: "HTML+CSS",
+    level: "advanced",
+    color: "#2662EA",
+  },
+  {
+    skill: "JavaScript",
+    level: "advanced",
+    color: "#EFD81D",
+  },
+  {
+    skill: "Web Design",
+    level: "advanced",
+    color: "#C3DCAF",
+  },
+  {
+    skill: "Git and GitHub",
+    level: "intermediate",
+    color: "#E84F33",
+  },
+  {
+    skill: "React",
+    level: "advanced",
+    color: "#60DAFB",
+  },
+  {
+    skill: "Svelte",
+    level: "beginner",
+    color: "#FF3B00",
+  },
+]
 
 export default function App() {
   return (
@@ -39,21 +71,31 @@ const SkillList = () => {
   return (
     <div>
       <ul className="skill-list">
-        <Skill skill="Javascript" bg="steelblue" pk="gg" />
+        {/* <Skill skill="Javascript" bg="steelblue" pk="gg" />
         {/* passing props as css properties value */}
-        <Skill skill="React" bg="#FAEBD7" />
+        {/* <Skill skill="React" bg="#FAEBD7" />
         <Skill skill="Tailwind CSS" bg="steelblue" />
         <Skill skill="HTML" bg="#FAEBD7" />
-        <Skill skill="CSS" bg="steelblue" />
+        <Skill skill="CSS" bg="steelblue" />  */}
+        {skills.map((sk) => {
+          return <Skill ski={sk} />
+        })}
       </ul>
     </div>
   )
 }
-function Skill({ skill, bg, pk }) {
+function Skill({ ski }) {
   return (
-    <li className="skill" style={{ backgroundColor: bg }}>
+    <li className="skill" style={{ backgroundColor: ski.color }}>
       {/* using props as css values */}
-      {skill} {pk}
+      {ski.skill}
+      <span>
+        {ski.level === "advanced"
+          ? "⭐️"
+          : ski.level === "intermediate"
+          ? "🔥"
+          : "🧑‍💻"}{" "}
+      </span>
     </li>
   )
 }
